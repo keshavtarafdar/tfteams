5caf98e90c3b45db5b41b00d2d85ebd1ca170776
Keshav <keshavtarafdar@gmail.com>
2025-07-27 22:04:27 -0400

    Update backend to accept POST from frontend and lookup summoner

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,49 @@
-import os
+import os, requests
 from dotenv import load_dotenv
-from fastapi import FastAPI
-import requests
-
-
-# const [msg, setMsg] = useState('Loading...')
-# useEffect(() => {
-#    fetch('/api/ping')
-#      .then(res => res.json())
-#      .then(data => setMsg(data.message))
-# }, [])
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 app = FastAPI()
 
-api_key = os.getenv("RIOT_API_KEY")
-region = "na1"
-BASE_URL = "https://{region}.api.riotgames.com"
+# The list of origins that are allowed to make requests to this backend
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 
-# @app is a path operation decorator - tells FastAPI that ping()
-# handles get operations at the path "/api/ping"
-@app.post("api/lookup")
-def ping():
-    return {"message": "pong"}
+# CORS allows frontend to communicate with backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # Allows all methods
+    allow_headers=["*"], # Allows all headers
+)
+
+# Define a data model 
+class SearchData(BaseModel):
+    gameName: str
+    tagLine: str
+
+API_KEY = os.getenv("RIOT_API_KEY")
+
+# Path option decorator that defines lookup() as handling requests to the route /api/lookup
+@app.post("/api/lookup")
+def lookup(data:SearchData):
+    base_url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/"
+    request_url = "{}{}/{}".format(base_url, data.gameName, data.tagLine)
+    headers = {
+        "X-Riot-Token": API_KEY
+    }
+
+    try:
+        riot_response = requests.get(request_url, headers=headers)
+        riot_response.raise_for_status() # Raises an HTTPError if request is unsuccessful
+    except requests.exceptions.RequestException as e: # Catches the HTTPError from above
+        if riot_response.status_code == 404:
+            raise HTTPException(status_code=404, detail=f"Riot account '{data.gameName}#{data.tagLine}' not found.")
+        raise HTTPException(status_code=500, detail=f"An error occurred with the Riot API: {e}")
+    
+    return riot_response.json()
